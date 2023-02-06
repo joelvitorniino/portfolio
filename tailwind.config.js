defeabab3a2bed41a7eceb488031ca62b10dbb5c
joelvitorniino <joelprofessor13@gmail.com>
2023-02-06 13:41:41 -0300

    style: create custom properties in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        '1.2em': '19.2px'
+      },
+      backgroundColor: {
+        'black-smooth': '#333'
+      },
+      margin: {
+        '3em': '4.8px',
+        '1.3em': '20.8px'
+      }
+    },
   },
   plugins: [],
 }
