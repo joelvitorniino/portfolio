156eefacdf8618c7c8dd38294bbda959c5088397
joelvitorniino <joelprofessor13@gmail.com>
2023-02-06 14:11:59 -0300

    feat: component Footer

---
+++ pages/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="flex items-center justify-center bg-black-smooth h-7 mt-1 border border-t-2 border-solid border-black-light text-white">
+            <p>
+                <span className="font-bold">Joel Vitor</span> &copy; 2023
+            </p>
+        </footer>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Footer />
+    </>
   )   
 }
