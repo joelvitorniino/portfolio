c0a0b642c986fda5f44559551b7010a9d23518b7
joelvitorniino <joelprofessor13@gmail.com>
2023-02-07 19:23:21 -0300

    fix: without border radius on p

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -5,7 +5,7 @@ export default function Card() {
     return (
         <div className="flex justify-center items-center flex-col pl-2em pr-1em mb-2em w-23% border-r-1em border-2 border-solid border-redPersonally shadow-solid bg-black-smooth text-white">
             <Image src="/image/portfolio.svg" width="120" height="120" alt="Portfolio Image" />
-            <p className="ml-4 mr-0 bg-redPersonally border-r-5 p-1.5 text-white flex justify-center items-center">1</p>
+            <p className="ml-4 mr-4 bg-redPersonally p-1.5 text-white flex justify-center items-center">#1</p>
             <h1 className="capitalize mb-4 text-sm-extra">Test</h1>
             <Link href="#" className="no-underline bg-white text-blackLight pr-.7em pl-1.2em border-r-5 font-bold duration-0.4s hover:bg-redPersonally hover:text-white">Details</Link>
         </div>
