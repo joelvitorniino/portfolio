e9a36f5bfbb68e8ebe4ce16455447da4bd71b1fe
joelvitorniino <joelprofessor13@gmail.com>
2023-02-07 19:01:49 -0300

    feat: component card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Card from "./components/Card";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Card />
       <Footer />
     </>
   )   
