84bb77deda7d8533cc94bdb0f1c5422fcf9f486f
joelvitorniino <joelprofessor13@gmail.com>
2023-02-08 17:48:26 -0300

    feat: new style footer

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
+
 export default function Footer() {
-    return (
-        <footer className="flex items-center justify-center bg-black-smooth h-7 mt-1 border border-t-2 border-solid border-black-light text-white">
-            <p>
-                <span className="font-bold">Joel Vitor</span> &copy; 2023
-            </p>
-        </footer>
-    );
+  return (
+    <>
+      <footer className="fixed bottom-0 p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 w-full">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          &copy; 2023{" "}
+          <Link
+            href="https://www.linkedin.com/in/joel-vitor-niino-campos-b319ba238/"
+            className="hover:underline"
+          >
+            Joel Vitor
+          </Link>
+          . All Rights Reserved.
+        </span>
+      </footer>
+    </>
+  );
 }
