e7224691c49195df47959f54842c2ddfd492e78c
joelvitorniino <joelprofessor13@gmail.com>
2023-02-08 17:01:00 -0300

    feat: props in card component

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -1,13 +1,55 @@
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 
-export default function Card() {
-    return (
-        <div className="flex justify-center items-center flex-col pl-2em pr-1em mb-2em w-23% border-r-1em border-2 border-solid border-redPersonally shadow-solid bg-black-smooth text-white">
-            <Image src="/image/portfolio.svg" width="120" height="120" alt="Portfolio Image" />
-            <p className="ml-4 mr-4 bg-redPersonally p-1.5 text-white flex justify-center items-center">#1</p>
-            <h1 className="capitalize mb-4 text-sm-extra">Test</h1>
-            <Link href="#" className="no-underline bg-white text-blackLight pr-.7em pl-1.2em border-r-5 font-bold duration-0.4s hover:bg-redPersonally hover:text-white">Details</Link>
-        </div>
-    );
+interface CardProps {
+    imageLink: string;
+    url: string;
+    title: string;
+    description: string;
+};
+
+export default function Card(props: CardProps) {
+  return (
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex justify-center items-center flex-col">
+      <Link href="#">
+        <Image
+          className="rounded-t-lg"
+          src={props.imageLink}
+          width="150"
+          height="150"
+          alt="Portfolio"
+        />
+      </Link>
+      <div className="p-5">
+        <Link href={props.url}>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {props.title}
+          </h5>
+        </Link>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {props.description}
+        </p>
+        <Link
+          href="#"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Details
+          <svg
+            aria-hidden="true"
+            className="w-4 h-4 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import Card from "./components/Card";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-wrap justify-between items-center max-w-5.3 ml-0 mr-auto">
-          <Card />
+      <div className="grid grid-cols-3 justify-between items-center max-w-5.3 ml-0 mr-auto">
+          <Card imageLink="/image/portfolio.svg" url="https://joelvitorniino.github.io" title="Project Tailwind" description="Project made with Tailwind!" key={null}  />
       </div>
     </>
   )   
