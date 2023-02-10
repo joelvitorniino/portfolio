bbd8b20800b0000b0c93a57b0abf208a3af955d4
joelvitorniino <joelprofessor13@gmail.com>
2023-02-10 16:04:40 -0300

    fix: correct path in project

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
             <title>Projects Page</title>
         </Head>
         <div className="grid grid-cols-3 justify-between items-center max-w-5.3 ml-0 mr-auto">
-          <Card imageLink="/image/portfolio.svg" url="https://joelvitorniino.github.io" title="Project Tailwind" description="Project made with Tailwind!" key={null}  />
+          <Card imageLink="/images/portfolio.svg" url="https://joelvitorniino.github.io" title="Project Tailwind" description="Project made with Tailwind!" key={null}  />
         </div>
         </>
     )
