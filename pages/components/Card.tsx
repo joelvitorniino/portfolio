eeb46ff0e902ae08e7bf5f7840f6c50e35d65ae6
joelvitorniino <joelprofessor13@gmail.com>
2023-02-11 17:45:04 -0300

    feat: update with more props

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+type SafeNumber = number | `${number}`;
+
 interface CardProps {
     imageLink: string;
+    width: SafeNumber;
+    height: SafeNumber;
     url: string;
     title: string;
     description: string;
@@ -16,22 +20,20 @@ export default function Card(props: CardProps) {
         <Image
           className="rounded-t-lg"
           src={props.imageLink}
-          width="150"
-          height="150"
+          width={props.width}
+          height={props.height}
           alt="Portfolio"
         />
       </Link>
-      <div className="p-5">
-        <Link href={props.url}>
+      <div className="p-7">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {props.title}
           </h5>
-        </Link>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {props.description}
         </p>
         <Link
-          href="#"
+          href={props.url}  
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Details
