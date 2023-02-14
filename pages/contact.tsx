12706ef647fb458a1228bc26317458f08a4019bf
joelvitorniino <joelprofessor13@gmail.com>
2023-02-13 21:19:08 -0300

    style: format contact code

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,32 +3,35 @@ import Link from "next/link";
 import { FaLinkedin, FaWhatsapp } from "react-icons/fa";
 
 export default function Contact() {
-    return (
-        <>
-        <Head>
-            <title>Contact Page</title>
-        </Head>
-        <div className="bg-gray-300 p-8">
-            <div className="mt-4">
-                <span className="inline-block m-1">
-                    <FaLinkedin size={45} className="inline" />
-                    <Link href="https://br.linkedin.com/in/joel-vitor-niino-campos-b319ba238" target="_blank">
-                        <p className="inline ml-2 font-semibold hover:font-bold">
-                            Joel Vitor Niino Campos
-                        </p>
-                    </Link>
-                </span>
+  return (
+    <>
+      <Head>
+        <title>Contact Page</title>
+      </Head>
+      <div className="bg-gray-300 p-8">
+        <div className="mt-4">
+          <span className="inline-block m-1">
+            <FaLinkedin size={45} className="inline" />
+            <Link
+              href="https://br.linkedin.com/in/joel-vitor-niino-campos-b319ba238"
+              target="_blank"
+            >
+              <p className="inline ml-2 font-semibold hover:font-bold">
+                Joel Vitor Niino Campos
+              </p>
+            </Link>
+          </span>
 
-                <span className="flex m-1">
-                    <FaWhatsapp size={45} className="inline" />
-                    <Link href="https://wa.me/+5521967401179" target="_blank">
-                        <p className="inline ml-2 font-semibold hover:font-bold">
-                            Joel Vitor - Whatsapp
-                        </p>
-                    </Link>
-                </span>
-            </div>
+          <span className="flex m-1">
+            <FaWhatsapp size={45} className="inline" />
+            <Link href="https://wa.me/+5521967401179" target="_blank">
+              <p className="inline ml-2 font-semibold hover:font-bold">
+                Joel Vitor - Whatsapp
+              </p>
+            </Link>
+          </span>
         </div>
-        </>
-    )
-};
+      </div>
+    </>
+  );
+}
