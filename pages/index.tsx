b9383fb3685b5e1edaef65bf8167e257237d1083
joelvitorniino <joelprofessor13@gmail.com>
2023-02-14 14:37:32 -0300

    feat: add responsive in text and move up element home

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,26 +32,27 @@ export default function Home() {
             className="w-32 h-32 rounded-full mb-4"
           />
           <h1 className="text-4xl font-bold mb-4">Joel Vitor</h1>
-          <h1 className="text-xl text-gray-700 mb-10">
+          <h1 className="text-xl iphone:text-center text-gray-700 mb-10">
             <Typewriter
               options={{
                 strings: compliments,
                 autoStart: true,
+                loop: true
               }}
             />
           </h1>
         </div>
-        <div className="flex flex-col md:flex-row justify-between w-64">
+        <div className="flex flex-col md:flex-row iphone:grid iphone:grid-cols-2 justify-between w-64">
           <Link
             href="https://github.com/joelvitorniino"
-            className="bg-gray-300 py-2 px-4 rounded-lg mb-4 md:mb-0 hover:bg-gray-400"
+            className="bg-gray-300 py-2 px-4 relative -top-12 mr-2 rounded-lg md:mb-0 hover:bg-gray-400"
           >
             <BsGithub className="w-7 h-7 inline-block" />
             <span className="ml-2">Github</span>
           </Link>
           <Link
             href="https://www.linkedin.com/in/joel-vitor-niino-campos-b319ba238/"
-            className="bg-gray-300 py-2 px-4 rounded-lg hover:bg-gray-400"
+            className="bg-gray-300 py-2 px-4 relative -top-12 rounded-lg hover:bg-gray-400"
           >
             <FaLinkedin className="w-7 h-7 inline-block" />
             <span className="ml-2">Linkedin</span>
