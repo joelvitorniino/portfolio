713b8baf68e9cd6726cf08018c73c5137af7339d
joelvitorniino <joelprofessor13@gmail.com>
2023-02-13 21:14:30 -0300

    feat: add icon on layout global

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }: React.PropsWithChildren) {
     return (
         <>
+            <Head>
+                <link rel="shortcut icon" href="/images/portfolio.ico" type="image/x-icon" />
+            </Head>
             <Navbar />
             <main className="min-h-vh">{children}</main>
             <Footer />
