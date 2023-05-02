09e81b936e51cf3c33fed1d39e74a99a564094e3
Joel Vitor Niino Campos <73134353+joelvitorniino@users.noreply.github.com>
2023-05-02 14:12:25 -0300

    feat: change age

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import Head from "next/head";
 
 const compliments = [
   "Me chamo Joel Vitor",
-  "Tenho 16 anos",
+  "Tenho 17 anos",
   "Sou Programador Full Stack",
   "Meu grande sonho é se tornar um grande desenvolvedor!",
   "Estudo programação há 3 anos!",
