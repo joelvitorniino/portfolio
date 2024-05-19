bb8e9e47fcc668b2ab5e6bcb6d379ac58ff60288
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-05-19 20:53:30 -0300

    build: migrate image to remotePatterns

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['cdn-icons-png.flaticon.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn-icons-png.flaticon.com',
+        port: '443',
+        pathname: '/icons/png/512/1000/solid/heart.png'
+      }
+    ],
     formats: ['image/avif', 'image/webp']
   }
 }
