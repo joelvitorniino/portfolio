54ee938d94f31802ceacd28a31709a2a380917dc
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-05-19 21:40:51 -0300

    feature: add all projects endpoint

---
+++ pages/api/projects.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../lib/prisma' 
+ 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const projects = await prisma.project.findMany();
+
+  res.status(200).json({ data: projects })
+}
