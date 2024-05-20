8f3acaf91ca24bed621b0ff6cd2264a801fdd497
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-05-20 14:21:28 -0300

    feature: implement api key on all projects endpoint

--- pages/api/projects.ts
+++ pages/api/projects.ts
@@ -5,7 +5,17 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const projects = await prisma.project.findMany();
+  const apiKey = req.headers['x-api-key'];
+  
+  if(apiKey !== process.env.API_KEY) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  };
 
-  res.status(200).json({ data: projects })
+  try {
+    const projects = await prisma.project.findMany();
+
+    res.status(200).json({ data: projects })
+  } catch(error) {
+    res.status(500).json({ error: 'Internal server error.' });
+  }
 }
