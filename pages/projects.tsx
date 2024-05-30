61861f3dfe1d3b0b5611ed5c5ad7a3d6021a727d
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-05-30 16:15:31 -0300

    feature: rectify error of render

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import Card from "./components/Card";
-import useSWR from 'swr'
+import useSWR from 'swr';
 import NextNProgress from 'nextjs-progressbar';
 
 interface Project {
   title: string;
   description: string;
   url: string;
-  imagelink: string;
+  imageLink: string;
   repository: string;
 }
 
@@ -15,21 +15,24 @@ interface Projects {
   data: Project[];
 }
 
+const fetcher = (input: RequestInfo) => fetch(input, {
+  headers: {
+    'x-api-key': process.env.NEXT_PUBLIC_API_KEY as string,
+    'Content-Type': 'application/json'
+  }
+}).then(res => res.json());
+
 export default function Projects() {
-  const fetcher = (input: RequestInfo) => fetch(input, {
-    headers: {
-      'x-api-key': process.env.NEXT_PUBLIC_API_KEY as string,
-      'Content-Type': 'application/json'
-    }
-  }).then(res => res.json())
   const { data, error } = useSWR<Projects>('/api/projects', fetcher);
+
+  console.log(data?.data);
 
   if (error) {
     return (
       <div>
         <p>Erro ao carregar os projetos.</p>
       </div>
-    )
+    );
   }
 
   if (!data) {
@@ -37,7 +40,7 @@ export default function Projects() {
       <div>
         <NextNProgress />
       </div>
-    )
+    );
   }
 
   return (
@@ -45,14 +48,16 @@ export default function Projects() {
       <Head>
         <title>Projects Page</title>
       </Head>
-      <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 justify-between md:justify-end items-center md:items-stretch max-w-5.3 ml-0 mr-auto">
+      <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 gap-4 max-w-5xl mx-auto p-4">
         {data.data.map(project => (
+          
+
           <Card
             key={project.title}
             title={project.title}
             description={project.description}
             url={project.url}
-            imageLink={project.imagelink}
+            imageLink={project.imageLink}
             repository={project.repository}
           />
         ))}
