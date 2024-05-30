aee34f99d86284b56ee86f9e27e1fa972fd35479
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-05-30 16:15:11 -0300

    feature: cards showed in the middle

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -19,7 +19,7 @@ export default function Card(props: CardProps) {
           src={props.imageLink}
           layout="fill"
           objectFit="cover"
-          alt="Portfolio"
+          alt={props.title}
         />
       </div>
       <div className="p-7 flex flex-col flex-grow">
@@ -31,44 +31,37 @@ export default function Card(props: CardProps) {
         </p>
         <div className="flex-grow"></div>
         <div className="flex">
-          <Link
-            href={props.url ?? ""}
-            className="flex-1 inline-flex items-center justify-center py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-bl-lg rounded-br-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Details
-            <svg
-              aria-hidden="true"
-              className="w-4 h-4 ml-2 -mr-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+          <Link href={props.url ?? ""} className="flex-1 inline-flex items-center justify-center py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-bl-lg rounded-br-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" passHref>
+              Details
+              <svg
+                aria-hidden="true"
+                className="w-4 h-4 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
           </Link>
-
-          <Link
-            href={props.repository ?? ""}
-            className="flex-1 inline-flex items-center justify-center py-3 ml-2 text-sm font-medium text-center text-white bg-gray-600 rounded-bl-lg rounded-br-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-700 dark:hover:bg-gray-800 dark:focus:ring-gray-600"
-          >
-            Repository
-            <svg
-              aria-hidden="true"
-              className="w-4 h-4 ml-2 -mr-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+          <Link href={props.repository ?? ""} className="flex-1 inline-flex items-center justify-center py-3 ml-2 text-sm font-medium text-center text-white bg-gray-600 rounded-bl-lg rounded-br-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-700 dark:hover:bg-gray-800 dark:focus:ring-gray-600" passHref>
+              Repository
+              <svg
+                aria-hidden="true"
+                className="w-4 h-4 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
           </Link>
         </div>
       </div>
