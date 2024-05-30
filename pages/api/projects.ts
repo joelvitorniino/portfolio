c6bae1cc15605317e9817a5009d2064554dfbce1
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-05-30 16:14:42 -0300

    feature: switch prisma to projects data from json

--- pages/api/projects.ts
+++ pages/api/projects.ts
@@ -1,20 +1,19 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from '../../lib/prisma' 
- 
+import type { NextApiRequest, NextApiResponse } from 'next';
+import projects from '../../data/projects.json';
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const apiKey = req.headers['x-api-key'];
-  
+
   if(apiKey !== process.env.API_KEY) {
     return res.status(401).json({ message: 'Unauthorized' });
-  };
+  }
 
   try {
-    const projects = await prisma.project.findMany();
-
-    res.status(200).json({ data: projects })
+    const { data } = projects;
+    res.status(200).json({ data });
   } catch(error) {
     res.status(500).json({ error: 'Internal server error.' });
   }
